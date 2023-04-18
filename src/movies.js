62b96b41c625a1bfeafad61892f7e9d8d4ebec79
Mich Seixas <mcseixas@gmail.com>
2023-04-18 23:40:00 +0200

    3 Jasmine hechos, lab no finalizado AÚN :)

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,34 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+  const allTheDirectors = moviesArray.map( (movie) => {
+    return movie.director;
+  });
+
+  return allTheDirectors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(movies) {
+    let filteredDirector = movies.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama"));
+    return filteredDirector.length;
+    };  
+    
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (moviesArray.length === 0){
+        return 0
+    }
+    let resultScores = moviesArray.reduce((acc, currMovie) => {
+        return acc + currMovie.score;
+    }, 0);
+    let average = resultScores/moviesArray.length;
+    
+    return Math.round(average * 100) / 100;
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
